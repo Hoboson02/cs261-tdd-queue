78a0ef0fbd922fd21fb9c252e87b3da21ff54b60
Ryan Earl <ryanleoearl@gmail.com>
2020-10-31 20:13:25 -0700

    Completed all Tests

--- queue.py
+++ queue.py
@@ -1,11 +1,19 @@
 # Queue: A queue.
 # Your implementation should pass the tests in test_queue.py.
-# YOUR NAME
+# Ryan Earl
 
 # Hint: pip3 install llist
-# from llist import sllist
-
+from llist import sllist
 class Queue:
 
-    pass
+    def __init__(self): 
+        self.data =sllist()
+    
+    def enqueue(self, value):
+        self.data.append(value)
 
+    def dequeue(self): 
+        return self.data.popleft()
+
+    def is_empty(self): 
+        return self.data.size == 0
